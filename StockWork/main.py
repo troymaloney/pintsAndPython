eae09b8d774a2617b59ae0c209c32180622e3bdf
ilragazzo <anthonyesanchez@yahoo.it>
2018-09-13 15:53:36 -0700

    made Comments
    Looks good, I made comments to aid me in my own developments.

--- StockWork/main.py
+++ StockWork/main.py
@@ -12,27 +12,27 @@ def main():
     stock = input("What stock would you like information about: ")
 
     while loopCount < maxLoop:
-        elapsed = time.time()
-        get_stock_CSV(stock)
-        killer = parse_stock_csv(stock)
-        loopCount=loopCount+1
-        elapsed = time.time()-elapsed
+        elapsed = time.time()       #sets elapsed to current time
+        get_stock_CSV(stock)        #API funtion call
+        killer = parse_stock_csv(stock)     #Parsing function call
+        loopCount=loopCount+1               #loop increment
+        elapsed = time.time()-elapsed       #sets elapsed time to loop duration 
         print("Loop iteration: {}".format(loopCount))
-        if killer:
+        if killer:      # True = error  
             break
         
         if loopCount != maxLoop:
             sleepCount = sleepCount+1
             time.sleep(60.-elapsed)
              
-    now = time.time()- now    
+    now = time.time()- now          #sets now to the main.py run time
     print("Total function run time: {:.43f} seconds".format(elapsed))
     print("Program run time total: {:.3f} seconds".format(now))
     print("Total times looped: {} times.".format(loopCount))
     print("Total times sleep called: {} times.".format(sleepCount))
 
 #IF __name__ not needed for python 3 and up, just a hold over XP
-if __name__ == "__main__":
+if __name__ == "__main__":      # I have no idea
     main()
 
 
