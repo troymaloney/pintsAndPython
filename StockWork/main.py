d119717746109266a0234be5f759281bdf0317a4
ilragazzo <anthonyesanchez@yahoo.it>
2018-09-18 15:47:43 -0700

    Updated main to call the plotting function
    I'm unsure if plot_ stocks should be called in the loop or not so I opted for not. it's still in beta  there probably needs to be tested some more. I'd like to see automatic updating so we aren't just waiting for the loop to finish. i'd also like for this to be a new window but im not there yet.

--- StockWork/main.py
+++ StockWork/main.py
@@ -1,10 +1,11 @@
 from getStockCSV import get_stock_CSV
 from parseStockCSV import parse_stock_csv
+from Plot import plot_stocks
 
 import time
 
 def main():
-    maxLoop = 10
+    maxLoop = 3 #390 time in a single day
     loopCount=0
     sleepCount=0
     now = time.time()   # get the time
@@ -31,11 +32,12 @@ def main():
     print("Total times looped: {} times.".format(loopCount))
     print("Total times sleep called: {} times.".format(sleepCount))
 
+    print("")
+    Prices = plot_stocks(stock) #calls plotting function
+    
+    
 #IF __name__ not needed for python 3 and up, just a hold over XP
 if __name__ == "__main__":      # I have no idea
     main()
 
 
-
-
-                 
