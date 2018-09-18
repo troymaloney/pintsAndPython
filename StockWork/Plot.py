5eb7867c5ffaa3aaea40c2c049a5286387b49115
ilragazzo <anthonyesanchez@yahoo.it>
2018-09-18 15:50:34 -0700

    Does what it says it does
    Beta version. future updates: opens a new window, has axes info, has dates affiliated with points

--- StockWork/Plot.py
+++ StockWork/Plot.py
@@ -6,44 +6,47 @@
 #  open file for reading
 #  read data 
 #  format data from string to float
-#  invert the data to most recent on top(?) 
 #  plot datas
 ##############################################
 
 import csv 
 from matplotlib import pyplot as plt
 
-with open ('CRONparsedStockData.txt') as csvfile:
-	readcsv =csv.reader(csvfile,delimiter=',')
-	Dates=[]
-	Prices=[]
-	n=0
+def plot_stocks(stock):
+    
 
-	#Skips the Header
-	header =next(readcsv)
-
-	#for loop starts here
-	for row in readcsv:
-
-		#gets the date from Row 7 and price from Row 5
-		PriceStr = row[7]
-		Price = float(PriceStr)
-		DateStr = row[5]
-		Date = float(DateStr)
+     with open ('CRONparsedStockData.txt') as csvfile:
+          readcsv =csv.reader(csvfile,delimiter=',')
+          Dates=[]
+          Prices=[]
+          n=0
+          
+          #for loop starts here
+          for row in readcsv:
+               #gets the date from Row 7 and price from Row 5
+               PriceStr = row[7]
+               Price = float(PriceStr)
+               DateStr = row[5]
+               Date = float(DateStr)
 		
-		#appends Price and date to the lists
-		Dates.append(Date)
-		Prices.append(Price)
+		        #appends Price and date to the lists
+               Dates.append(Date)
+               Prices.append(Price)
 
-		#for loop control:
-		n=n+1
-		print (n)
-		
-		if n >600:
-			break
+		     
+               #for loop control:
+		     
+               n=n+1
+               #print (n)
+               
+               if n >390:
+                    break
 
-print (Dates)
-print (Prices)
+     #print (Dates)
+     #print (Prices)
 
-plt.plot(Prices)
-plt.show()
+     RoughGriaffe = plt.plot(Prices)
+     #plt.show()
+     
+     return(Prices)
+     
