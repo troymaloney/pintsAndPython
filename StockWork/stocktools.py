1bbe1eecf46a5a0c42da5ee24f0e9bee7898a881
Troy <trmaloney94@gmail.com>
2018-11-17 16:04:06 -0800

    attributes are only assigned if header exists; class argument is now file instead of dir

--- StockWork/stocktools.py
+++ StockWork/stocktools.py
@@ -3,7 +3,6 @@
 #Last edited by: Troy Maloney
 #Last edited on: 11-17-18
 
-
 import csv
 import os
 import glob
@@ -11,18 +10,22 @@ import numpy as np
 
 class StockDataset:
     """
-        When given a path to a directory containing stock data gathered 
+        When given a file containing stock data gathered 
     with main.py, this class will read in the stored data and create a 
     dataset object. This object will have attributes containing the data
     columns. This could help make our plotting, algorithm, and GUI code
     a little less cumbersome. 
-    
-    !!! Only a directory with ONE data text file can be passed into this 
-        class right now
 
     
-    SYNTAX:
-    stock = StockDataset("/path/to/file") 
+    How to use:
+    
+    from stocktools import StockDataset
+    stock = StockDataset("data.txt")
+    
+    If data file is not in current working directory, use data_dir keyword
+        to specify the directory where data is stored.
+        ex: stock = StockDataset("data.txt", data_dir="path/to/file")
+        
     Set 'verbose' option to True to print record of actions to terminal
     
     
@@ -48,26 +51,21 @@ class StockDataset:
         
     
     PLANNED FEATURES:
-    1. The option to read in data that was taken within a range of dates
-        instead of just reading in an entire directory
-    2. Read in dirs that contain more than one data text file
-    3. A safeguard against data from more than one different stock being 
-        read in using symbol tag on each API call
+    1. The option to read in multiple data files and string them together
+    2. A way to update with current data as API calls happen
 
     """
 
-    def __init__(self, data_dir, verbose=True): #verbose False by default in release
+    def __init__(self, fname, data_dir=None, verbose=True): #verbose False by default in release
 
-        if data_dir[-1] != os.sep: # make sure separator character is appended to end of
-            data_dir += os.sep     # of directroy string 
-    
-        self.data_dir = data_dir # set attribute to return name of directory
-        
-        if verbose:
-            print("Reading in data from "+self.data_dir)
-        
-        # Grab only the first stock data file in the directory for now
-        dataFile = glob.glob(data_dir+'*parsedStockData.txt')[0]
+        if not data_dir: # if no directory specified:
+            data_dir = os.getcwd() + os.sep # look for datafile in current directory
+
+        else: # if directory is specified 
+            if data_dir[-1] != os.sep: # make sure separator character is appended to end of
+                data_dir += os.sep     # of directory string 
+
+        dataFile = data_dir + fname
         
         if verbose:
             print("Data table file is called "+dataFile)
@@ -83,15 +81,21 @@ class StockDataset:
                 
         self.dataTable_full = np.array(dataTable_full)
         self.headers = headers
-        self.symbol = dataTable_full[0][0]
         
-        self.prices = self.dataTable_full[:,headers.index('price')].astype(np.float)
-        self.volumes = self.dataTable_full[:,headers.index('volume')].astype(np.float)
-        self.changes = self.dataTable_full[:,headers.index('change')].astype(np.float)
-        self.calltimes = self.dataTable_full[:,headers.index('callTime')]
-        
-        change_percents = self.dataTable_full[:,headers.index('changePercent')]
-        self.change_percents = np.array([float(p.strip('%') ) for p in change_percents] )
+#       Check if data type exists before creating attributes
+        if 'symbol' in headers:
+            self.symbol = dataTable_full[0][headers.index('symbol')]
+        if 'price' in headers:
+            self.prices = self.dataTable_full[:,headers.index('price')].astype(np.float)
+        if 'volume' in headers:
+            self.volumes = self.dataTable_full[:,headers.index('volume')].astype(np.float)
+        if 'change' in headers:
+            self.changes = self.dataTable_full[:,headers.index('change')].astype(np.float)
+        if 'callTime' in headers:
+            self.calltimes = self.dataTable_full[:,headers.index('callTime')]
+        if 'changePercent' in headers:
+            change_percents = self.dataTable_full[:,headers.index('changePercent')]
+            self.change_percents = np.array([float(p.strip('%') ) for p in change_percents] )
 
 ## FOR TESTING -- COMMENT or DELETE in release ##
 #cron = StockDataset('/home/pi/Documents/pintsAndPython/StockWork')
